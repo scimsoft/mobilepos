a4988d0495bad4f397f73e8fb9040e042ad83007
Gerrit <gerrit.sevilla@gmail.com>
2020-05-12 01:18:55 +0200

    jquery in blades

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -75,3 +75,16 @@
     </div>
 </div>
 @endsection
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        jQuery(document).ready(function () {
+
+
+        });
+
+    </script>
+
+
+
+@stop
